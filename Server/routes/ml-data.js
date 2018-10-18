757a841ec74d9619a9fbb712e312f9571c8a7781
SAM10795 <silversoulsam@gmail.com>
2018-10-18 22:56:00 +0530

    Modified mldata

--- Server/routes/ml-data.js
+++ Server/routes/ml-data.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+
+function proc(jdata) {
+	var alert="";
+	if('alerts' in jdata)
+	{
+		alert = jdata.alerts;
+	}
+	var precipint = 0.0;	//max is 25
+	var i;
+	for(i=0;i<7;i++)
+	{
+		precipint+=jdata.daily.data[i].precipIntensityMax;
+	}
+	var flood = precipint>50;
+	return {'alert':alert,'precipint':precipint,'flood':flood}
+	
+}
 // const spawn = require('child_process').spawn;
 
 // let runPy = new Promise(function(success, nosuccess) {
@@ -34,16 +51,32 @@ router.get('/', (req, res, next) => {
       err: 'Please Specify a Location',
     });
   }
-
-  var url = `https://api.darksky.net/forecast/9191d936fc03d4c50faebb90012b5400/${location}?exclude=currently,minutely,hourly,flags`;
-  request(url, { json: true }, (err, apiRes, body) => {
-    if (err) {
-      return res.status(500).json({
-        err: err,
-      });
-    }
-    return res.status(200).json(body);
+  var locs = location.split(',');
+  var locations = [parseFloat(locs[0]).toString()+","+parseFloat(locs[1]).toString(),
+  			(parseFloat(locs[0])+0.02).toString()+","+parseFloat(locs[1]).toString(), 
+  			parseFloat(locs[0]).toString()+","+(parseFloat(locs[1])+0.02).toString(), 
+  			(parseFloat(locs[0])-0.02).toString()+","+parseFloat(locs[1]).toString(), 
+  			parseFloat(locs[0]).toString()+","+(parseFloat(locs[1])-0.02).toString(), 
+  			(parseFloat(locs[0])+0.02).toString()+","+(parseFloat(locs[1])+0.02).toString(), 
+  			(parseFloat(locs[0])+0.02).toString()+","+(parseFloat(locs[1])-0.02).toString(), 
+  			(parseFloat(locs[0])-0.02).toString()+","+(parseFloat(locs[1])+0.02).toString(), 
+  			(parseFloat(locs[0])-0.02).toString()+","+(parseFloat(locs[1])-0.02).toString()]; 			
+  var i;
+  var arr = []
+  for(i=0;i<locations.length;i++)
+  {
+  	var url = `https://api.darksky.net/forecast/9191d936fc03d4c50faebb90012b5400/${locations[i]}?exclude=currently,minutely,hourly,flags`;
+  	request(url, { json: true }, (err, apiRes, body) => {
+    	if (err) {
+      	return res.status(500).json({
+        	err: err,
+      	});
+    	}
+    	console.log(proc(body));
+  	});
+  }
+  	return res.status(200).json('DONE');
   });
-});
+
 
 module.exports = router;
