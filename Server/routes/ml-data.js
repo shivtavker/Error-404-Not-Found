e32d1a43ff379a38a3a030dbef080f01a1e99f19
shivtavker <tavkershiv@gmail.com>
2018-10-19 11:56:55 +0530

    Added Multiple API Calls

--- Server/routes/ml-data.js
+++ Server/routes/ml-data.js
@@ -1,23 +1,32 @@
+const async = require('async');
 var express = require('express');
 var router = express.Router();
 var request = require('request');
 
 function proc(jdata) {
-	var alert="";
-	if('alerts' in jdata)
-	{
-		alert = jdata.alerts;
-	}
-	var precipint = 0.0;	//max is 25
-	var i;
-	for(i=0;i<7;i++)
-	{
-		precipint+=jdata.daily.data[i].precipIntensityMax;
-	}
-	var flood = precipint>50;
-	return {'alert':alert,'precipint':precipint,'flood':flood}
-	
+  var alert = '';
+  if ('alerts' in jdata) {
+    alert = jdata.alerts;
+  }
+  var precipint = 0.0; //max is 25
+  var i;
+  for (i = 0; i < 7; i++) {
+    precipint += jdata.daily.data[i].precipIntensityMax;
+  }
+  var flood = precipint > 50;
+  return { alert: alert, precipint: precipint, flood: flood };
 }
+
+function httpGet(url, callback) {
+  const options = {
+    url: url,
+    json: true,
+  };
+  request(options, (err, apiRes, body) => {
+    callback(err, body);
+  });
+}
+
 // const spawn = require('child_process').spawn;
 
 // let runPy = new Promise(function(success, nosuccess) {
@@ -52,31 +61,49 @@ router.get('/', (req, res, next) => {
     });
   }
   var locs = location.split(',');
-  var locations = [parseFloat(locs[0]).toString()+","+parseFloat(locs[1]).toString(),
-  			(parseFloat(locs[0])+0.02).toString()+","+parseFloat(locs[1]).toString(), 
-  			parseFloat(locs[0]).toString()+","+(parseFloat(locs[1])+0.02).toString(), 
-  			(parseFloat(locs[0])-0.02).toString()+","+parseFloat(locs[1]).toString(), 
-  			parseFloat(locs[0]).toString()+","+(parseFloat(locs[1])-0.02).toString(), 
-  			(parseFloat(locs[0])+0.02).toString()+","+(parseFloat(locs[1])+0.02).toString(), 
-  			(parseFloat(locs[0])+0.02).toString()+","+(parseFloat(locs[1])-0.02).toString(), 
-  			(parseFloat(locs[0])-0.02).toString()+","+(parseFloat(locs[1])+0.02).toString(), 
-  			(parseFloat(locs[0])-0.02).toString()+","+(parseFloat(locs[1])-0.02).toString()]; 			
-  var i;
-  var arr = []
-  for(i=0;i<locations.length;i++)
-  {
-  	var url = `https://api.darksky.net/forecast/9191d936fc03d4c50faebb90012b5400/${locations[i]}?exclude=currently,minutely,hourly,flags`;
-  	request(url, { json: true }, (err, apiRes, body) => {
-    	if (err) {
-      	return res.status(500).json({
-        	err: err,
-      	});
-    	}
-    	console.log(proc(body));
-  	});
+  var locations = [
+    parseFloat(locs[0]).toString() + ',' + parseFloat(locs[1]).toString(),
+    (parseFloat(locs[0]) + 0.02).toString() +
+      ',' +
+      parseFloat(locs[1]).toString(),
+    parseFloat(locs[0]).toString() +
+      ',' +
+      (parseFloat(locs[1]) + 0.02).toString(),
+    (parseFloat(locs[0]) - 0.02).toString() +
+      ',' +
+      parseFloat(locs[1]).toString(),
+    parseFloat(locs[0]).toString() +
+      ',' +
+      (parseFloat(locs[1]) - 0.02).toString(),
+    (parseFloat(locs[0]) + 0.02).toString() +
+      ',' +
+      (parseFloat(locs[1]) + 0.02).toString(),
+    (parseFloat(locs[0]) + 0.02).toString() +
+      ',' +
+      (parseFloat(locs[1]) - 0.02).toString(),
+    (parseFloat(locs[0]) - 0.02).toString() +
+      ',' +
+      (parseFloat(locs[1]) + 0.02).toString(),
+    (parseFloat(locs[0]) - 0.02).toString() +
+      ',' +
+      (parseFloat(locs[1]) - 0.02).toString(),
+  ];
+  var urls = [];
+  for (var i = 0; i < locations.length; i++) {
+    var url = `https://api.darksky.net/forecast/9191d936fc03d4c50faebb90012b5400/${locations[
+      i
+    ]}?exclude=currently,minutely,hourly,flags`;
+    urls.push(url);
   }
-  	return res.status(200).json('DONE');
-  });
 
+  async.map(urls, httpGet, (err, body) => {
+    if (err) {
+      return res.status(500).json({
+        error: err,
+      });
+    }
+    return res.status(200).json(body);
+  });
+});
 
 module.exports = router;
